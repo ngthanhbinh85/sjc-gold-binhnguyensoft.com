c7cd21ad56bf3e82e36456b28bfbb3efd94c5faa
Binh Nguyen <52373935+ngthanhbinh85@users.noreply.github.com>
2026-09-08 11:56:55 +0700

    Update

---
+++ prefs.js
@@ -0,0 +1,171 @@
+/*
+ * Widget giá vàng SJC
+ *
+ * Copyright (C) 2026 Binh Nguyen (binhnguyensoft.com)
+ *
+ */
+
+import Adw from 'gi://Adw';
+import Gtk from 'gi://Gtk?version=4.0';
+
+import {ExtensionPreferences} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
+
+export default class SjcGoldPreferences extends ExtensionPreferences {
+    fillPreferencesWindow(window) {
+        window.set_default_size(600, -1);
+        this._settings = this.getSettings();
+
+        const page = new Adw.PreferencesPage({
+            title: 'Widget giá vàng SJC',
+            icon_name: 'preferences-desktop-display-symbolic',
+        });
+
+        // Vị trí
+        const positionGroup = new Adw.PreferencesGroup({
+            title: 'Vị trí',
+            description: 'Thay đổi vị trí xuất hiện của widget',
+        });
+
+        // Trái
+        const leftKey = this._settings.settings_schema.get_key('left');
+        
+        const left = new Gtk.Adjustment({
+            lower: 0,
+            upper: 10000,
+            step_increment: 5,
+            value: this._settings.get_int('left'),
+        });
+
+        const leftRow = new Adw.SpinRow({
+            title: leftKey.get_summary(),
+            subtitle: leftKey.get_description(),
+            adjustment: left,
+            digits: 0,
+            numeric: true,
+            snap_to_ticks: true,
+        });
+
+        leftRow.connect('notify::value', row => {
+            const value = Math.round(row.get_value());
+            if (this._settings.get_int('left') !== value)
+                this._settings.set_int('left', value);
+        });
+
+        // Trên
+        const topKey = this._settings.settings_schema.get_key('top');
+        
+        const top = new Gtk.Adjustment({
+            lower: 0,
+            upper: 10000,
+            step_increment: 5,
+            value: this._settings.get_int('top'),
+        });
+
+        const topRow = new Adw.SpinRow({
+            title: topKey.get_summary(),
+            subtitle: topKey.get_description(),
+            adjustment: top,
+            digits: 0,
+            numeric: true,
+            snap_to_ticks: true,
+        });
+
+        topRow.connect('notify::value', row => {
+            const value = Math.round(row.get_value());
+            if (this._settings.get_int('top') !== value)
+                this._settings.set_int('top', value);
+        });
+        
+        // Chu kỳ cập nhật
+        const durationGroup = new Adw.PreferencesGroup({
+            title: 'Cập nhật',
+            description: 'Thay đổi chu kỳ cập nhật giá',
+        });
+        
+        const durationKey = this._settings.settings_schema.get_key('duration');
+
+        const duration = new Gtk.Adjustment({
+            lower: 5,
+            upper: 300,
+            step_increment: 5,
+            value: this._settings.get_int('duration'),
+        });
+
+        const durationRow = new Adw.SpinRow({
+            title: durationKey.get_summary(),
+            subtitle: durationKey.get_description(),
+            adjustment: duration,
+            digits: 0,
+            numeric: true,
+            snap_to_ticks: true,
+        });
+
+        durationRow.connect('notify::value', row => {
+            const value = Math.round(row.get_value());
+            if (this._settings.get_int('duration') !== value)
+                this._settings.set_int('duration', value);
+        });
+
+        positionGroup.add(leftRow);
+        positionGroup.add(topRow);
+        durationGroup.add(durationRow);
+        page.add(positionGroup);
+        page.add(durationGroup);
+        window.add(page);
+        this._addAboutButton(window);
+
+    }
+
+    _addAboutButton(window) {
+        const headerBar = this._findHeaderBar(window);
+        if (!headerBar)
+            return;
+
+        const aboutButton = new Gtk.Button({
+            icon_name: 'open-menu-symbolic',
+            tooltip_text: 'About',
+            valign: Gtk.Align.CENTER,
+        });
+        aboutButton.add_css_class('flat');
+        aboutButton.connect('clicked', () => this._showAbout(window));
+
+        headerBar.pack_end(aboutButton);
+    }
+
+    _findHeaderBar(widget) {
+        if (widget instanceof Adw.HeaderBar)
+            return widget;
+
+        for (let child = widget.get_first_child(); child;
+            child = child.get_next_sibling()) {
+            const headerBar = this._findHeaderBar(child);
+            if (headerBar)
+                return headerBar;
+        }
+
+        return null;
+    }
+
+    _showAbout(window) {
+        const params = {
+            application_name: this.metadata.name,
+            developer_name: 'Bình Nguyễn',
+            version: this.metadata['version-name'],
+            website: 'https://www.binhnguyensoft.com',
+            issue_url: `${this.metadata.url.replace(/\/+$/, '')}/issues`,
+        };
+
+        if (Adw.AboutDialog) {
+            const about = new Adw.AboutDialog(params);
+            about.present(window);
+        } else {
+            const about = new Adw.AboutWindow({
+                ...params,
+                transient_for: window,
+                modal: true,
+            });
+            about.present();
+        }
+    }
+
+}
